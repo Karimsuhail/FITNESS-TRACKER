d1bdc56367e01bf4c16e3dfd20f94e4e86656f11
Karimsuhail <karimsuhail18@gmail.com>
2024-11-21 23:59:40 +0530

    cors change

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ import UserRoutes from "./routes/User.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: "https://fitness-tracker-c23f.onrender.com", // Replace with your frontend URL
+  credentials: true, // If using cookies or Authorization headers
+};
+
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
